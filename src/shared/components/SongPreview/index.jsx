786bb97864609530e8dd8e0bf9019e32108756fb
Julian Boilen <julianboilen@gmail.com>
2018-03-03 20:50:49 -0500

    Reduce page shifting due to song previews on mobile

--- src/shared/components/SongPreview/index.jsx
+++ src/shared/components/SongPreview/index.jsx
@@ -10,28 +10,38 @@ type Props = {
 };
 
 type State = {
-  uri : ?string
+  uri : ?string,
+  loaded: boolean
 };
 
 
 export default class SongPreview extends React.Component<Props, State> {
   constructor(props : Props) {
     super(props);
-    this.state = { uri: null };
+    this.state = { uri: null, loaded: false };
   }
   componentDidMount() {
     findSongUri(this.props.song)
       .then(uri => this.setState({ uri }))
-      .catch(e => console.error('Searching spotify failed', e));
+      .catch(e => console.error('Searching spotify failed', e))
+      .then(() => {this.setState({ loaded: true });});
   }
 
   render() {
-    const { uri } = this.state;
+    const { uri, loaded } = this.state;
+    const className = cx(this.props.className);
+    if (!loaded) {
+      // Since it will probably load successfully,
+      // take up space while loading to reduce shifting
+      return (<div className={className}>
+        <div style={{ width: '100%', height: '100%' }} />
+      </div>);
+    }
     if (!uri) {
       return null;
     }
     return (<iframe
-      className={cx(this.props.className)}
+      className={className}
       src={'https://open.spotify.com/embed?uri=' + uri}
       frameBorder="0"
       // eslint-disable-next-line react/no-unknown-property
