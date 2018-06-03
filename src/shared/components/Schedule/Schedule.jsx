1f9967ad95b86a0cdd7bcbf5e5e828e0bb6cbd4f
Julian Boilen <julianboilen@gmail.com>
2018-06-03 13:35:17 -0400

    CSS-snap points based mobile view with day buttons.

--- src/shared/components/Schedule/Schedule.jsx
+++ src/shared/components/Schedule/Schedule.jsx
@@ -10,6 +10,8 @@ import WeeklyDayTime from 'util/time/WeeklyDayTime';
 import formatLocalTime from 'util/time/formatLocalTime';
 import DayNames from 'util/time/DayNames';
 import cx from 'classnames';
+import ReactDOM from 'react-dom';
+import throttle from 'lodash/throttle';
 
 type Props<T> = {
   blocks: BlockData<T>[],
@@ -54,58 +56,103 @@ const createHourlyBlocks = (dayStartsAt: LocalTime, latestTime: LocalTime): Bloc
 };
 
 
-export default function Schedule<T>(props: Props<T>) {
-  const { blocks, height, dayStartsAt, dayClassName } = props;
+export default class Schedule<T> extends React.Component<Props<T>, {activeDay: DayOfWeek}> {
+  weekEl: HTMLElement;
+  updateDayListener: (*) => void;
+  dayElements: {[$Values<DayOfWeek>]: ?Day<T>}
+  constructor(props: Props<T>) {
+    super(props);
+    this.state = { activeDay: new Date().getDay() + 1 };
+  }
 
-  // Allocate blocks to days.
-  // The start of the day can be customized.
-  // Events occuring before the start of the day are shown on the previous day.
-  let earliestTime = LocalTime.MAX;
-  let latestTime = dayStartsAt;
-  const blocksByDay = {};
-  Object.values(DayOfWeek).forEach((dayOfWeek: DayOfWeek) => {
-    blocksByDay[dayOfWeek] = [];
-  });
-  blocks.forEach(block => {
-    const allocateToYesterday = block.start.time.compareTo(dayStartsAt) < 0;
-    const allocatedDay = allocateToYesterday ?
-      addWeekdays(block.start.weekday, -1) : block.start.weekday;
-    if (!allocateToYesterday && block.start.time.compareTo(earliestTime) < 0) {
-      earliestTime = block.start.time;
-    }
-    if (allocateToYesterday && block.end.time.compareTo(latestTime) < 0) {
-      latestTime = block.end.time;
-    }
-    blocksByDay[allocatedDay].push(block);
-  });
+  componentDidMount() {
+    this.scrollToDay(this.state.activeDay);
+    this.weekEl.addEventListener('scroll', this.updateDayListener = throttle(this.updateActiveDay.bind(this), 300));
+  }
 
-  return (
-    <div className={cx(stylesheet.container, props.className)} style={{ height: height }}>
-      <div className={stylesheet.dayNames}>
-        {Object.values(DayOfWeek).map((dayOfWeek: DayOfWeek) => <div key={dayOfWeek}>{DayNames[dayOfWeek]}</div>)}
-      </div>
-      <div className={stylesheet.weekTimesContainer} >
-        <Day
-          blocks={createHourlyBlocks(earliestTime, latestTime)}
-          start={WeeklyDayTime.of(DayOfWeek.MONDAY, earliestTime)}
-          end={WeeklyDayTime.of(DayOfWeek.TUESDAY, latestTime)}
-          renderBlock={({ block: blockData }) => formatLocalTime(blockData.alternatives.default[0].data, true)}
-          className={stylesheet.times}
-        />
-        <div className={stylesheet.week}>
+  componentWillUnmount() {
+    this.weekEl.removeEventListener('scroll', this.updateDayListener);
+  }
+
+  updateActiveDay() {
+    for (let day of Object.values(DayOfWeek)) {
+      const dayNode = ((ReactDOM.findDOMNode(this.dayElements[day]): any): HTMLElement);
+      const dayWidth = dayNode.getBoundingClientRect().width;
+      if (dayNode && (dayNode.offsetLeft - this.weekEl.offsetLeft + dayWidth / 2) >= this.weekEl.scrollLeft) {
+        this.setState({ activeDay: day });
+        return;
+      }
+    }
+  }
+
+  scrollToDay(day: $Values<DayOfWeek>) {
+    const dayNode = ((ReactDOM.findDOMNode(this.dayElements[day]): any): HTMLElement);
+    this.weekEl.scrollLeft = dayNode.offsetLeft - this.weekEl.offsetLeft;
+  }
+
+  render() {
+    const { blocks, height, dayStartsAt, dayClassName, renderBlock } = this.props;
+    const { activeDay } = this.state;
+    this.dayElements = {};
+
+    // Allocate blocks to days.
+    // The start of the day can be customized.
+    // Events occuring before the start of the day are shown on the previous day.
+    let earliestTime = LocalTime.MAX;
+    let latestTime = dayStartsAt;
+    const blocksByDay = {};
+    Object.values(DayOfWeek).forEach((dayOfWeek: DayOfWeek) => {
+      blocksByDay[dayOfWeek] = [];
+    });
+    blocks.forEach(block => {
+      const allocateToYesterday = block.start.time.compareTo(dayStartsAt) < 0;
+      const allocatedDay = allocateToYesterday ?
+        addWeekdays(block.start.weekday, -1) : block.start.weekday;
+      if (!allocateToYesterday && block.start.time.compareTo(earliestTime) < 0) {
+        earliestTime = block.start.time;
+      }
+      if (allocateToYesterday && block.end.time.compareTo(latestTime) < 0) {
+        latestTime = block.end.time;
+      }
+      blocksByDay[allocatedDay].push(block);
+    });
+
+    return (
+      <div className={cx(stylesheet.container, this.props.className)} style={{ height: height }}>
+        <div className={stylesheet.dayNames}>
           {Object.values(DayOfWeek).map((dayOfWeek: DayOfWeek) =>
-            (
-              <Day
-                key={dayOfWeek}
-                blocks={blocksByDay[dayOfWeek]}
-                start={WeeklyDayTime.of(dayOfWeek, earliestTime)}
-                end={WeeklyDayTime.of((dayOfWeek + 1) % 7, latestTime)}
-                renderBlock={props.renderBlock}
-                className={dayClassName}
-              />
-            ))}
+            (<div
+              key={dayOfWeek}
+              onClick={() => this.scrollToDay(dayOfWeek)}
+              className={cx({ [stylesheet.active]: dayOfWeek === activeDay })}>
+              {DayNames[dayOfWeek]}
+            </div>))}
+        </div>
+        <div className={stylesheet.weekTimesContainer} >
+          <Day
+            blocks={createHourlyBlocks(earliestTime, latestTime)}
+            start={WeeklyDayTime.of(DayOfWeek.MONDAY, earliestTime)}
+            end={WeeklyDayTime.of(DayOfWeek.TUESDAY, latestTime)}
+            renderBlock={({ block: blockData }) => formatLocalTime(blockData.alternatives.default[0].data, true)}
+            className={stylesheet.times}
+          />
+          <div className={stylesheet.week} ref={(el) => {if (el) {this.weekEl = el;}}}>
+            {Object.values(DayOfWeek).map((dayOfWeek: DayOfWeek) =>
+              (
+                <Day
+                  ref={(dayEl) => {this.dayElements[dayOfWeek] = dayEl;}}
+                  key={dayOfWeek}
+                  blocks={blocksByDay[dayOfWeek]}
+                  start={WeeklyDayTime.of(dayOfWeek, earliestTime)}
+                  end={WeeklyDayTime.of((dayOfWeek + 1) % 7, latestTime)}
+                  renderBlock={renderBlock}
+                  className={dayClassName}
+                />
+              ))}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+
+  }
 }
