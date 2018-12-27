a0147450aa826b43d259f176e83e7bd212bef4fe
Julian Boilen <julianboilen@gmail.com>
2018-12-26 21:52:18 -0500

    Expose a function to render custom play buttons
    Will use for special shows featured on home page (outside of app)

--- src/wowd/WOWDComponents.js
+++ src/wowd/WOWDComponents.js
@@ -7,6 +7,8 @@ import GlobalPlayer from './screens/GlobalPlayer';
 import PlayButton from 'components/PlayButton';
 import LiveTrack from 'LiveTrack';
 import ShowSchedule from './screens/ShowSchedule';
+import TrackManager from 'TrackManager';
+import moment from 'moment';
 
 const renderApp = (container, App) => {
   const render = Component => {
@@ -70,4 +72,40 @@ export function toggleLiveTrack() {
   } else {
     LiveTrack.play();
   }
+}
+
+const catalog = {};
+
+/**
+ * Render a custom play button for an arbitrary audio file.
+ *
+ * Caveat: it is assumed that the same file won't be rendered multiple times with different metadata.
+ *
+ * @param {Node} container node to render in
+ * @param {Object} options
+ * @param {string} options.url audio URL
+ * @param {string} options.showName Name of show
+ * @param {Date} options.onAirAt airing start time
+ * @param {Object[]} options.djs list of Djs on the show
+ *  A dj has a 'name' and optionally an 'id' and 'imageUrl'.
+ * @param {string} options.theme button style.
+ *  'dark', 'light', or 'primary'.
+ * @param {string} options.size button size
+ *  'small' or 'large'
+ */
+export function renderArchivePlayButton(container, options) {
+  const metadata = {
+    isArchive: true,
+    showName: options.showName,
+    djs: options.djs || [],
+    onAirAt: moment(options.onAirAt)
+  };
+
+  if (!(options.url in catalog)) {
+    catalog[options.url] = TrackManager.createTrack(options.url, metadata);
+  }
+
+  ReactDOM.render(
+    <PlayButton track={catalog[options.url]} theme={options.theme} size={options.size} />,
+    container);
 }
